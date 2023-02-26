64a237194ea3e6238a2cc1a03aa29b691bb17bd3
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2023-02-26 20:04:15 -0300

    Feat: Fundamentos - Input de dados

--- inhere/src/screens/Home/style.ts
+++ inhere/src/screens/Home/style.ts
@@ -15,5 +15,13 @@ export const styles = StyleSheet.create({
   eventDate: {
     color: '#6B6B6B',
     fontSize: 16
+  }, 
+  input:{
+    height: 56,
+    backgroundColor: '#1F1E25',
+    borderRadius: 8,
+    color: '#fff',
+    padding: 16,
+    fontSize:16,
   }
 })
