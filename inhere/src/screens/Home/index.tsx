e5080a80c0b7d36562efb5ce4761888ac393ff55
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2023-02-26 22:39:29 -0300

    Feat: Fundamentos - Lista de participantes

--- inhere/src/screens/Home/index.tsx
+++ inhere/src/screens/Home/index.tsx
@@ -1,4 +1,4 @@
-import { ScrollView,Text, TextInput, TouchableOpacity, View } from "react-native";
+import { FlatList,Text, TextInput, TouchableOpacity, View } from "react-native";
 
 import { Participant } from "../../components/Participant";
 
@@ -53,16 +53,24 @@ export  function Home() {
         </TouchableOpacity>
       </View>
 
-      <ScrollView showsVerticalScrollIndicator={false}>
-        {participants.map(participant => (
-          <Participant 
-            key={participant}
-            name={participant}
-            onRemove={()=> handleParticipantRemove("Daniel")}
+      <FlatList 
+        data={participants}
+        keyExtractor={(item) => item}
+        renderItem={({item}) =>(
+          <Participant  
+            key={item}
+            name={item}
+            onRemove={() => handleParticipantRemove("Rodrigo")} 
           />
-        ))}
-        
-      </ScrollView>
+        )}
+        showsVerticalScrollIndicator={false}
+        ListEmptyComponent={()=>(
+          <Text style={styles.listEmptyText}>
+            Ninguém chegou no evento ainda? Adicione participantes a sua lista de presença.
+          </Text>
+        )}
+      
+      />
     </View>
   )
 }
